e192e914e639fe4bee3ae0e8f1da16037a1014cb
Lakshay Hasija <lakshaylear.n@gmail.com>
2024-02-26 13:12:40 +0530

    Create sum_of_2_numbers.py

---
+++ Problem_2/sum_of_2_numbers.py
@@ -0,0 +1,9 @@
+def sum_of_2_numbers(a,b) :
+    return a + b
+
+a = int(input("Enter Number 1"))
+
+b = int(input("Enter Number 2"))
+
+res = sum_of_2_numbers(a,b)
+print(f"The sum of {a} & {b} is {res}")
